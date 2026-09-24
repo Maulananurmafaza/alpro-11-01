9ffdd6bdef46ca865db19b19201fdfea9f68dc12
maaulananurmafaza <maulananurmafaza793@gmail.com>
2026-09-24 08:56:38 +0700

    initial commit

---
+++ praktikum/02-bahasa-pemrograman-go/unguided/cacahuang/cacahuang.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	var uang int
+	fmt.Println("Msukan nominal uang")
+	fmt.Scan(&uang)
+
+	sepuluhRibu := uang / 10000
+	sisa := uang % 10000
+
+	limaRibu := sisa / 5000
+	sisa = sisa % 5000
+
+	seRibu := sisa / 1000
+
+	fmt.Printf("Sepuluh Ribu = %d, Lima Ribu = %d, Seribu = %d\n", sepuluhRibu, limaRibu, seRibu)
+
+}
